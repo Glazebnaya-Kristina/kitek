366fd1981c8d235bf1c9ed4094ec1ffad3ea2f13
Glazebnaya-Kristina <46931114+Glazebnaya-Kristina@users.noreply.github.com>
2020-02-13 15:58:14 +0200

    1. Сверстана секция advantages 2. Исправлен паддинг у секции about-us 3. Изменена толщнна подчекивание ссылки при ховере navigation

--- assets/dist/js/home.js
+++ assets/dist/js/home.js
@@ -1,15 +1,34 @@
 $(document).ready(function () {
-   $('.about-us__carousel').slick({
-     mobileFirst: true,
-     prevArrow: '<button type="button" class="arrow-carousel arrow-carousel--oval arrow-carousel--prev about-us__arrow-carousel about-us__arrow-carousel--prev"><i class="icon-arrow"></i></button>',
-     nextArrow: '<button type="button" class="arrow-carousel arrow-carousel--oval arrow-carousel--next about-us__arrow-carousel about-us__arrow-carousel--next"><i class="icon-arrow"></i></button>',
-     responsive: [
-       {
-         breakpoint: 767,
-         settings: "unslick"
-       },
-     ]
-   });
+  $('.about-us__carousel').slick({
+    mobileFirst: true,
+    prevArrow: '<button type="button" class="arrow-carousel arrow-carousel--oval arrow-carousel--prev about-us__arrow-carousel about-us__arrow-carousel--prev"><i class="icon-arrow"></i></button>',
+    nextArrow: '<button type="button" class="arrow-carousel arrow-carousel--oval arrow-carousel--next about-us__arrow-carousel about-us__arrow-carousel--next"><i class="icon-arrow"></i></button>',
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: "unslick"
+      },
+    ]
+  });
+
+  $('.advantages__carousel').slick({
+    mobileFirst: true,
+    prevArrow: '<button type="button" class="arrow-carousel arrow-carousel--oval arrow-carousel--prev advantages__arrow-carousel advantages__arrow-carousel--prev"><i class="icon-arrow"></i></button>',
+    nextArrow: '<button type="button" class="arrow-carousel arrow-carousel--oval arrow-carousel--next advantages__arrow-carousel advantages__arrow-carousel--next"><i class="icon-arrow"></i></button>',
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        },
+      },
+      {
+        breakpoint: 1023,
+        settings: "unslick"
+      },
+    ]
+  });
 });
 
 
