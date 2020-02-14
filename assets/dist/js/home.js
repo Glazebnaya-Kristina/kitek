1a52477dd98557273d25e2eb6dd013aa2e1bab48
Glazebnaya-Kristina <46931114+Glazebnaya-Kristina@users.noreply.github.com>
2020-02-14 13:52:32 +0200

    1. Сверстана секция our-process 1. Сверстана секция case-studies

--- assets/dist/js/home.js
+++ assets/dist/js/home.js
@@ -29,6 +29,37 @@ $(document).ready(function () {
       },
     ]
   });
+
+  $('.our-process__carousel').slick({
+    mobileFirst: true,
+    prevArrow: '<button type="button" class="arrow-carousel arrow-carousel--oval arrow-carousel--prev our-process__arrow-carousel our-process__arrow-carousel--prev"><i class="icon-arrow"></i></button>',
+    nextArrow: '<button type="button" class="arrow-carousel arrow-carousel--oval arrow-carousel--next our-process__arrow-carousel our-process__arrow-carousel--next"><i class="icon-arrow"></i></button>',
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        },
+      },
+      {
+        breakpoint: 1023,
+        settings: "unslick"
+      },
+    ]
+  });
+
+  $('.case-studies__carousel').slick({
+    mobileFirst: true,
+    prevArrow: '<button type="button" class="arrow-carousel arrow-carousel--oval arrow-carousel--prev case-studies__arrow-carousel case-studies__arrow-carousel--prev"><i class="icon-arrow"></i></button>',
+    nextArrow: '<button type="button" class="arrow-carousel arrow-carousel--oval arrow-carousel--next case-studies__arrow-carousel case-studies__arrow-carousel--next"><i class="icon-arrow"></i></button>',
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: "unslick"
+      },
+    ]
+  });
 });
 
 
