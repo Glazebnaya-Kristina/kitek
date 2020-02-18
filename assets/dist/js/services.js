446957b1aa30a7da846bd929851886cd8bb0cfc9
Glazebnaya-Kristina <46931114+Glazebnaya-Kristina@users.noreply.github.com>
2020-02-18 17:10:59 +0200

    1. Удален ненужный js

--- assets/dist/js/services.js
+++ assets/dist/js/services.js
@@ -1,15 +1,5 @@
 $(document).ready(function () {
-  // $('.about-us__carousel').slick({
-  //   mobileFirst: true,
-  //   prevArrow: '<button type="button" class="arrow-carousel arrow-carousel--oval arrow-carousel--prev about-us__arrow-carousel about-us__arrow-carousel--prev"><i class="icon-arrow"></i></button>',
-  //   nextArrow: '<button type="button" class="arrow-carousel arrow-carousel--oval arrow-carousel--next about-us__arrow-carousel about-us__arrow-carousel--next"><i class="icon-arrow"></i></button>',
-  //   responsive: [
-  //     {
-  //       breakpoint: 767,
-  //       settings: "unslick"
-  //     },
-  //   ]
-  // });
+
 });
 
 
