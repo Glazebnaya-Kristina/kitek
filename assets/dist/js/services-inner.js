ae0aa4b368034e78e04c656600dc0d124a70abec
Glazebnaya-Kristina <46931114+Glazebnaya-Kristina@users.noreply.github.com>
2020-02-20 14:55:59 +0200

    Написан скрипт перехода со страницы services на конкретный таб в services-inner

--- assets/dist/js/services-inner.js
+++ assets/dist/js/services-inner.js
@@ -1,20 +1,28 @@
 $(document).ready(function () {
   // табы
-  $('ul.tabs__caption').each(function (i) {
-    var storage = localStorage.getItem('tab' + i);
-    if (storage) {
-      $(this).find('li').removeClass('tabs-active').eq(storage).addClass('tabs-active')
-        .closest('div.tabs').find('li.tabs__content').removeClass('tabs-active').eq(storage).addClass('tabs-active');
-    }
-  });
+  // $('ul.tabs__caption').each(function (i) {
+  //   var storage = localStorage.getItem('tab' + i);
+  //   if (storage) {
+  //     $(this).find('li').removeClass('tabs-active').eq(storage).addClass('tabs-active')
+  //       .closest('div.tabs').find('li.tabs__content').removeClass('tabs-active').eq(storage).addClass('tabs-active');
+  //   }
+  // });
 
   $('ul.tabs__caption').on('click', 'li:not(.tabs-active)', function () {
     $(this)
       .addClass('tabs-active').siblings().removeClass('tabs-active')
       .closest('div.tabs').find('li.tabs__content').removeClass('tabs-active').eq($(this).index()).addClass('tabs-active')
       .parents().find('.services-inner__case-list').removeClass('services-inner__case-list--active').eq($(this).index()).addClass('services-inner__case-list--active');
-    var ulIndex = $('ul.tabs__caption').index($(this).parents('ul.tabs__caption'));
-    localStorage.removeItem('tab' + ulIndex);
-    localStorage.setItem('tab' + ulIndex, $(this).index());
+    // var ulIndex = $('ul.tabs__caption').index($(this).parents('ul.tabs__caption'));
+    // localStorage.removeItem('tab' + ulIndex);
+    // localStorage.setItem('tab' + ulIndex, $(this).index());
   });
+
+
+  var localValue = localStorage.getItem('clicked-tab').replace('#', '');
+  $('.tabs__caption li[id=' + localValue + ']').addClass('tabs-active').siblings().removeClass('tabs-active')
+    .closest('.tabs').find('.tabs__content[data-id=' + localValue + ']').addClass('tabs-active').siblings().removeClass('tabs-active')
+    .closest('.tabs').find('.services-inner__case-list[data-id=' + localValue + ']')
+    .addClass('services-inner__case-list--active').siblings().removeClass('services-inner__case-list--active')
+
 });
--- assets/dist/js/services.js
+++ assets/dist/js/services.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
 
+  jQuery(function(){
+    var key = 'clicked-tab' ;
+    jQuery('.about-us__item-link').on('click', function(){
+      localStorage.setItem(key, jQuery(this).data('tab'));
+    });
+  });
+
 });
 
 
