0b30cdb9d87f25ca2f24eca723044be23c3a9e09
Glazebnaya-Kristina <46931114+Glazebnaya-Kristina@users.noreply.github.com>
2020-02-24 12:43:27 +0200

    Изменен скрипт блока показать еще

--- assets/dist/js/about-us.js
+++ assets/dist/js/about-us.js
@@ -49,21 +49,62 @@ $(document).ready(function () {
     ]
   });
 
-  $(".js-more").click(function() {
 
-    var elem = $(".js-more span").text();
-    if (elem == "Show more") {
-      //Stuff to do when btn is in the read more state
-      $(".js-more span").text("Show less");
-      $(".description__content p:not(:first-child)").slideDown();
-      $(this).addClass('js-more--active')
-    } else {
-      //Stuff to do when btn is in the read less state
+  // $(".js-more").click(function() {
+  //
+  //   var elem = $(".js-more span").text();
+  //   if (elem == "Show more") {
+  //     //Stuff to do when btn is in the read more state
+  //     $(".js-more span").text("Show less");
+  //     $(".description__content p:not(:first-child)").slideDown();
+  //     $(this).addClass('js-more--active')
+  //   } else {
+  //     //Stuff to do when btn is in the read less state
+  //     $(".js-more span").text("Show more");
+  //     $(".description__content p:not(:first-child)").slideUp();
+  //     $(this).removeClass('js-more--active');
+  //   }
+  // });
+
+
+  var defaultHeight;
+  var width = $(window).width();
+  if (width < 479) {
+    defaultHeight = 480;
+  } else if (width > 479 && width < 768){
+    defaultHeight = 180;
+  } else if (width > 768 && width < 1024){
+    defaultHeight = 210;
+  }
+
+  var text = $(".description__content");
+  var textHeight = text[0].scrollHeight;
+  var button = $(".js-more");
+  text.css({"max-height": defaultHeight, "overflow": "hidden"});
+
+
+  button.on("click", function(){
+    var newHeight = 0;
+    if (text.hasClass("active")) {
+      newHeight = defaultHeight;
+      text.removeClass("active");
+
       $(".js-more span").text("Show more");
-      $(".description__content p:not(:first-child)").slideUp();
       $(this).removeClass('js-more--active');
+    } else {
+      newHeight = textHeight;
+      text.addClass("active");
+      // $(".js-more").text("Read Less");
+      $(".js-more span").text("Show less");
+      $(this).addClass('js-more--active')
     }
+
+    text.animate({
+      "max-height": newHeight
+    }, 500);
   });
+
+
 
   jcf.replaceAll();
 
--- assets/dist/js/careers.js
+++ assets/dist/js/careers.js
@@ -1,19 +1,56 @@
 $(document).ready(function () {
 
-  $(".js-more").click(function() {
+  // $(".js-more").click(function() {
+  //
+  //   var elem = $(".js-more span").text();
+  //   if (elem == "Show more") {
+  //     //Stuff to do when btn is in the read more state
+  //     $(".js-more span").text("Show less");
+  //     $(".careers__content p:not(:first-child)").slideDown();
+  //     $(this).addClass('js-more--active')
+  //   } else {
+  //     //Stuff to do when btn is in the read less state
+  //     $(".js-more span").text("Show more");
+  //     $(".careers__content p:not(:first-child)").slideUp();
+  //     $(this).removeClass('js-more--active');
+  //   }
+  // });
 
-    var elem = $(".js-more span").text();
-    if (elem == "Show more") {
-      //Stuff to do when btn is in the read more state
-      $(".js-more span").text("Show less");
-      $(".careers__content p:not(:first-child)").slideDown();
-      $(this).addClass('js-more--active')
-    } else {
-      //Stuff to do when btn is in the read less state
+  var defaultHeight;
+  var width = $(window).width();
+  if (width < 479) {
+    defaultHeight = 513;
+  } else if (width > 479 && width < 768){
+    defaultHeight = 180;
+  } else if (width > 768 && width < 1024){
+    defaultHeight = 210;
+  }
+
+  var text = $(".careers__content");
+  var textHeight = text[0].scrollHeight;
+  var button = $(".js-more");
+  text.css({"max-height": defaultHeight, "overflow": "hidden"});
+
+
+  button.on("click", function(){
+    var newHeight = 0;
+    if (text.hasClass("active")) {
+      newHeight = defaultHeight;
+      text.removeClass("active");
+
       $(".js-more span").text("Show more");
-      $(".careers__content p:not(:first-child)").slideUp();
       $(this).removeClass('js-more--active');
+    } else {
+      newHeight = textHeight;
+      text.addClass("active");
+      $(".js-more span").text("Show less");
+      $(this).addClass('js-more--active')
     }
+
+    text.animate({
+      "max-height": newHeight
+    }, 500);
+
   });
 
 
